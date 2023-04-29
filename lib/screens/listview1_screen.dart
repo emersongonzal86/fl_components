c319852d2eae56142fd45a73dab36cfd9e8d04ec
emerson <emersongonzal86@gmail.com>
2023-04-29 12:30:26 -0500

    Creating ListTile

--- lib/screens/listview1_screen.dart
+++ lib/screens/listview1_screen.dart
@@ -1,6 +1,13 @@
 import 'package:flutter/material.dart';
 
 class Listview1Screen extends StatelessWidget {
+  final options = const [
+    'Megaman',
+    'Metal Gear',
+    'Super Smash',
+    'Final Fantasy'
+  ];
+
   const Listview1Screen({Key? key}) : super(key: key);
 
   @override
@@ -8,12 +15,13 @@ class Listview1Screen extends StatelessWidget {
     return Scaffold(
       appBar: AppBar(title: const Text('Listview Tipo 1')),
       body: ListView(
-        children: const[
-          Text('Hola Mundo'),
-          Text('Hola Mundo'),
-          Text('Hola Mundo'),
-          Text('Hola Mundo'),
-          Text('Hola Mundo'),
+        children: [
+          ...options
+              .map((game) => ListTile(
+                    title: Text(game),
+                    trailing: const Icon(Icons.arrow_forward_ios_outlined),
+                  ))
+              .toList()
         ],
       ),
     );
