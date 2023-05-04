df13ca1ae066cf6689153b6cecc09d83598082d5
emerson <emersongonzal86@gmail.com>
2023-05-03 21:18:51 -0500

    Card Widget

--- lib/screens/card_screen.dart
+++ lib/screens/card_screen.dart
@@ -1,15 +1,30 @@
 import 'package:flutter/material.dart';
 
+import '../theme/app_theme.dart';
+
 class CardScreen extends StatelessWidget {
-   
   const CardScreen({Key? key}) : super(key: key);
-  
+
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
-      body: Center(
-         child: Text('CardScreen'),
-      ),
-    );
+    return Scaffold(
+        appBar: AppBar(
+          title: const Text('Card Widget'),
+        ),
+        body: ListView(
+          padding: const EdgeInsets.symmetric(horizontal: 20, vertical: 10),
+          children: [
+            Card(
+              child: Column(children: const [
+                ListTile(
+                  leading:
+                      Icon(Icons.photo_album_outlined, color: AppTheme.primary),
+                  title: Text('Soy un título'),
+                  subtitle: Text('Lorem culpa veniam exercitation laboris cupidatat non laborum velit ipsum occaecat nisi.'),
+                )
+              ]),
+            )
+          ],
+        ));
   }
 }
