2f6f2be39ec742a90b190492e668bc1b7b80ce87
emerson <emersongonzal86@gmail.com>
2023-05-04 19:51:08 -0500

    Adjusting the image size

--- lib/widgets/custom_card_type_2.dart
+++ lib/widgets/custom_card_type_2.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:fl_components/theme/app_theme.dart';
 
 class CustomCardType2 extends StatelessWidget {
   const CustomCardType2({super.key});
@@ -6,14 +7,25 @@ class CustomCardType2 extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Card(
-        elevation: 10,
+        clipBehavior: Clip.antiAlias,
+        shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(18)),
+        elevation: 30,
+        shadowColor: AppTheme.primary.withOpacity(0.5),
         child: Column(
-          children: const [
-            FadeInImage(
+          children: [
+            const FadeInImage(
               image: NetworkImage(
                   'https://photographylife.com/wp-content/uploads/2017/01/What-is-landscape-photography-960x640.jpg'),
               placeholder: AssetImage('assets/jar-loading.gif'),
-            )
+              width: double.infinity,
+              height: 230,
+              fit: BoxFit.cover,
+              fadeInDuration: const Duration(milliseconds: 300),
+            ),
+            Container(
+                alignment: AlignmentDirectional.centerEnd,
+                padding: const EdgeInsets.only(right: 20, top: 10, bottom: 10),
+                child: Text('Un hermoso paisaje'))
           ],
         ));
   }
