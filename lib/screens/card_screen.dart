008ab1bcf08f371faae4699ca0500fdfc56b96f5
emerson <emersongonzal86@gmail.com>
2023-05-11 20:37:59 -0500

    Arguments to widgets

--- lib/screens/card_screen.dart
+++ lib/screens/card_screen.dart
@@ -15,7 +15,22 @@ class CardScreen extends StatelessWidget {
           children: const [
             CustomCardType1(),
             SizedBox(height: 10),
-            CustomCardType2()
+            CustomCardType2(
+                imageUrl:
+                    'https://photographylife.com/wp-content/uploads/2017/01/What-is-landscape-photography-960x640.jpg'),
+            SizedBox(height: 20),
+            CustomCardType2(
+              name: null,
+              imageUrl:
+                  'https://images.pexels.com/photos/388415/pexels-photo-388415.jpeg',
+            ),
+            SizedBox(height: 20),
+            CustomCardType2(
+              name: 'Un hermoso paisaje',
+              imageUrl:
+                  'https://images.pexels.com/photos/268533/pexels-photo-268533.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1',
+            ),
+            SizedBox(height: 100),
           ],
         ));
   }
--- lib/widgets/custom_card_type_2.dart
+++ lib/widgets/custom_card_type_2.dart
@@ -2,7 +2,11 @@ import 'package:flutter/material.dart';
 import 'package:fl_components/theme/app_theme.dart';
 
 class CustomCardType2 extends StatelessWidget {
-  const CustomCardType2({super.key});
+  //propiedades
+  final String imageUrl;
+  final String? name;
+
+  const CustomCardType2({super.key, required this.imageUrl, this.name});
 
   @override
   Widget build(BuildContext context) {
@@ -13,19 +17,23 @@ class CustomCardType2 extends StatelessWidget {
         shadowColor: AppTheme.primary.withOpacity(0.5),
         child: Column(
           children: [
-            const FadeInImage(
-              image: NetworkImage(
-                  'https://photographylife.com/wp-content/uploads/2017/01/What-is-landscape-photography-960x640.jpg'),
-              placeholder: AssetImage('assets/jar-loading.gif'),
+            FadeInImage(
+              image: NetworkImage(imageUrl),
+              placeholder: const AssetImage('assets/jar-loading.gif'),
               width: double.infinity,
               height: 230,
               fit: BoxFit.cover,
               fadeInDuration: const Duration(milliseconds: 300),
             ),
-            Container(
-                alignment: AlignmentDirectional.centerEnd,
-                padding: const EdgeInsets.only(right: 20, top: 10, bottom: 10),
-                child: Text('Un hermoso paisaje'))
+
+            // con el if puedo validar si es nulo, esto funciona en Dart se puede cambiar  el  child: Text( name ?? 'No Title' )
+            // por child: Text( name ) genera error pero se le cooca (name!) indicandole a flutter que no es un error
+            if (name != null)
+              Container(
+                  alignment: AlignmentDirectional.centerEnd,
+                  padding:
+                      const EdgeInsets.only(right: 20, top: 10, bottom: 10),
+                  child: Text(name ?? 'No Title'))
           ],
         ));
   }
