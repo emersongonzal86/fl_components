65fcb4fcaaa6566600b991d8536a1bbce46e2589
emerson <emersongonzal86@gmail.com>
2023-06-17 16:42:24 -0500

    centralize the style of inputs

--- lib/screens/inputs_screen.dart
+++ lib/screens/inputs_screen.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 
+import '../widgets/widgets.dart';
+
 class InputScreen extends StatelessWidget {
   const InputScreen({Key? key}) : super(key: key);
 
@@ -7,45 +9,17 @@ class InputScreen extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
         appBar: AppBar(
-          title: Text('Inputs and Forms'),
+          title: const Text('Inputs and Forms'),
         ),
-        body: SingleChildScrollView(
+        body: const SingleChildScrollView(
           child: Padding(
-              padding: const EdgeInsets.symmetric(horizontal: 20, vertical: 10),
+              padding:  EdgeInsets.symmetric(horizontal: 20, vertical: 10),
               child: Column(
                 children: [
-                  TextFormField(
-                    autofocus: false,
-                    initialValue: '',
-                    textCapitalization: TextCapitalization.words,
-                    onChanged: (value) {
-                      print('value: $value');
-                    },
-                    validator: (value) {
-                      if (value == null) return 'Este campo es requerido';
-                      return value.length < 3 ? 'Mínimo de 3 letras' : null;
-                    },
-                    autovalidateMode: AutovalidateMode.onUserInteraction,
-                    decoration: const InputDecoration(
-                        hintText: 'Nombre del usuario',
-                        labelText: 'Nombre',
-                        helperText: 'Sólo letras',
-                        // prefixIcon: Icon( Icons.verified_user_outlined),
-                        suffixIcon: Icon(Icons.group_outlined),
-                        icon: Icon(Icons.assignment_ind_outlined),
-                        focusedBorder: OutlineInputBorder(
-                          borderSide: BorderSide(
-                           color: Colors.green
-                          )
-                        ),
-                        border: OutlineInputBorder(
-                            borderRadius: BorderRadius.only(
-                          bottomLeft: Radius.circular(10),
-                          topRight: Radius.circular(10),
-                        ))),
-                  )
+                   CustomInputField( labelText: 'Nombre', hintText: 'Nombre del Usuario', )
                 ],
               )),
         ));
   }
 }
+
--- lib/theme/app_theme.dart
+++ lib/theme/app_theme.dart
@@ -26,8 +26,29 @@ class AppTheme {
                 shape:const StadiumBorder(),
                 elevation:0
                 ),
-                )
-);     
+                ),
+
+// inputdecorationTheme
+ inputDecorationTheme:  const InputDecorationTheme(
+  floatingLabelStyle: TextStyle( color: primary ),
+  enabledBorder: OutlineInputBorder(
+    borderSide: BorderSide( color: primary ),
+    borderRadius: BorderRadius.only(bottomLeft: Radius.circular(10), topRight: Radius.circular(10) )
+  ),
+
+  focusedBorder: OutlineInputBorder(
+    borderSide: BorderSide( color: primary ),
+    borderRadius: BorderRadius.only(bottomLeft: Radius.circular(10), topRight: Radius.circular(10) )
+  ),
+
+  border: OutlineInputBorder(
+      borderRadius: BorderRadius.only(bottomLeft: Radius.circular(10), topRight: Radius.circular(10) )
+  ),
+
+ )               
+); 
+
+
 
   //Tema oscuro
 
