174e34cecd001fb729486f2f95619188252dd326
emerson <emersongonzal86@gmail.com>
2023-06-17 19:36:09 -0500

    DropdownButtonFormField

--- lib/screens/inputs_screen.dart
+++ lib/screens/inputs_screen.dart
@@ -7,7 +7,6 @@ class InputScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-
     final GlobalKey<FormState> myFormKey = GlobalKey<FormState>();
 
     final Map<String, String> formValues = {
@@ -30,34 +29,50 @@ class InputScreen extends StatelessWidget {
                 child: Column(
                   children: [
                     CustomInputField(
-                        labelText: 'Nombre', hintText: 'Nombre del Usuario', formProperty: 'first_name', formValues: formValues),
+                        labelText: 'Nombre',
+                        hintText: 'Nombre del Usuario',
+                        formProperty: 'first_name',
+                        formValues: formValues),
                     const SizedBox(height: 30),
                     CustomInputField(
                         labelText: 'Apellido',
-                        hintText: 'Apellido del Usuario', formProperty: 'last_name', formValues: formValues),
+                        hintText: 'Apellido del Usuario',
+                        formProperty: 'last_name',
+                        formValues: formValues),
                     const SizedBox(height: 30),
                     CustomInputField(
-                      labelText: 'Correo',
-                      hintText: 'Correo del Usuario',
-                      keyboardType: TextInputType.emailAddress, formProperty: 'email', formValues: formValues
-                    ),
+                        labelText: 'Correo',
+                        hintText: 'Correo del Usuario',
+                        keyboardType: TextInputType.emailAddress,
+                        formProperty: 'email',
+                        formValues: formValues),
                     const SizedBox(height: 30),
                     CustomInputField(
-                      labelText: 'Contraseña',
-                      hintText: 'Contraseña del Usuario',
-                      obscureText: true, formProperty: 'password', formValues: formValues
-                    ),
+                        labelText: 'Contraseña',
+                        hintText: 'Contraseña del Usuario',
+                        obscureText: true,
+                        formProperty: 'password',
+                        formValues: formValues),
                     const SizedBox(height: 30),
+                    DropdownButtonFormField(
+                      value: 'Admin',
+                      items: const [
+                      DropdownMenuItem(value: 'Admin', child: Text('Admin')),
+                      DropdownMenuItem(value: 'Superuser', child: Text('Superuser')),
+                      DropdownMenuItem(value: 'Developer', child: Text('Developer')),
+                      DropdownMenuItem(value: 'Jr.Developer', child: Text('Jr.Developer')),
+                    ], onChanged: (value) {
+                      print(value);
+                      formValues['role']= value ?? 'Admin';
+                    }),
                     ElevatedButton(
                       child: const SizedBox(
                           width: double.infinity,
-                          child: Center(child: Text('Guardar'))
-                          ),
+                          child: Center(child: Text('Guardar'))),
                       onPressed: () {
-                        if ( !myFormKey.currentState!.validate() ){
-                      
-                      //codigo para ocultar el teclado al tocar el boton
-                            FocusScope.of(context).requestFocus( FocusNode());
+                        if (!myFormKey.currentState!.validate()) {
+                          //codigo para ocultar el teclado al tocar el boton
+                          FocusScope.of(context).requestFocus(FocusNode());
 
                           print('Formulario no válido');
                           return;
