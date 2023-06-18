44bcb13670cbd218db2e98f1da3d1a80f391d1c4
emerson <emersongonzal86@gmail.com>
2023-06-18 17:35:25 -0500

    Checkbox and switches

--- lib/screens/slider_screen.dart
+++ lib/screens/slider_screen.dart
@@ -10,6 +10,7 @@ class SliderScreen extends StatefulWidget {
 
 class _SliderScreenState extends State<SliderScreen> {
   double _sliderValue = 100;
+  bool _sliderEnabled = true;
 
   @override
   Widget build(BuildContext context) {
@@ -17,31 +18,64 @@ class _SliderScreenState extends State<SliderScreen> {
         appBar: AppBar(
           title: const Text('Sliders and Checks'),
         ),
-        body: SingleChildScrollView(
-          child: Column(
-            children: [
-              //adaptative es una funcion qu permite que el widget se comporte de manera natural a la plataforma en el que trabaje
-              Slider.adaptive(
-                  min: 50,
-                  max: 400,
-                  activeColor: AppTheme.primary,
-                  //dividir el slider con puntos de anclaje
-                  //divisions: 10,
-                  value: _sliderValue,
-                  onChanged: (value) {
-                    _sliderValue = value;
-                    setState(() {});
-                  }),
+        body: Column(
+          children: [
+            //adaptative es una funcion qu permite que el widget se comporte de manera natural a la plataforma en el que trabaje
+            Slider.adaptive(
+                min: 50,
+                max: 400,
+                activeColor: AppTheme.primary,
+                //dividir el slider con puntos de anclaje
+                //divisions: 10,
+                value: _sliderValue,
+                onChanged: _sliderEnabled
+                    ? (value) {
+                        _sliderValue = value;
+                        setState(() {});
+                      }
+                    : null),
 
-              Image(
-                image: const NetworkImage(
-                    'https://w7.pngwing.com/pngs/10/777/png-transparent-batman-cartoon-illustration-batman-toonseum-drawing-cartoon-batman-v-superman-heroes-superhero-fictional-character-thumbnail.png'),
-                fit: BoxFit.contain,
-                width: _sliderValue,
+            // Checkbox(
+            //     value: _sliderEnabled,
+            //     onChanged: (value) {
+            //       _sliderEnabled = value ?? true;
+            //       setState(() {});
+            //     }),
+
+            // Switch(value: _sliderEnabled,
+            //  onChanged: ((value) => setState(() {
+            //    _sliderEnabled = value;})
+            //    ),
+
+            CheckboxListTile(
+                activeColor: AppTheme.primary,
+                title: const Text('Habilitar el slider'),
+                value: _sliderEnabled,
+                onChanged: (value) => setState(() {
+                      _sliderEnabled = value ?? true;
+                    })),
+
+            SwitchListTile.adaptive(
+                activeColor: AppTheme.primary,
+                title: const Text('Habilitar el slider'),
+                value: _sliderEnabled,
+                onChanged: (value) => setState(() {
+                      _sliderEnabled = value ?? true;
+                    })),
+
+            const AboutListTile(),        
+
+            Expanded(
+              child: SingleChildScrollView(
+                child: Image(
+                  image: const NetworkImage(
+                      'https://w7.pngwing.com/pngs/10/777/png-transparent-batman-cartoon-illustration-batman-toonseum-drawing-cartoon-batman-v-superman-heroes-superhero-fictional-character-thumbnail.png'),
+                  fit: BoxFit.contain,
+                  width: _sliderValue,
+                ),
               ),
-              const SizedBox(height: 50)
-            ],
-          ),
+            ),
+          ],
         ));
   }
 }
